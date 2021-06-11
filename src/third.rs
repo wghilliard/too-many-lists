e40a2bdcbecfa97ccc436f04b0a3da972a8d7e4d
Grayson Hilliard <grsn.hilliard@gmail.com>
2021-06-11 10:05:47 -0700

    adds shared reference iter

--- src/third.rs
+++ src/third.rs
@@ -32,6 +32,25 @@ impl<T> List<T> {
     pub fn head(&self) -> Option<&T> {
         self.head.as_ref().map(|node| &node.elem)
     }
+
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
 }
 
 #[cfg(test)]
@@ -57,5 +76,17 @@ mod test {
 
         let list = list.tail();
         assert_eq!(list.head(), None);
+    }
+
+    #[test]
+    fn iter() {
+        let list = List::new().append(1).append(2).append(3);
+
+        let mut iter = list.iter();
+        let list = list.tail();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+        assert_eq!(iter.next(), None);
     }
 }
